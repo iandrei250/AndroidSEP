81e3d31903c1b1343dfb52bfd4d93776266ad9f0
GeanyFynnetzze <45146025+GeanyFynnetzze@users.noreply.github.com>
2020-04-15 15:16:19 +0200

    Nav + Chart

--- app/src/main/java/com/example/spacestationui/MainActivity.java
+++ app/src/main/java/com/example/spacestationui/MainActivity.java
@@ -1,19 +1,34 @@
 package com.example.spacestationui;
 
+import android.graphics.Color;
+import android.os.Bundle;
+
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.os.Bundle;
+//Bar Chart
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+
+
+
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
+    BarChart mpBarChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +43,94 @@ public class MainActivity extends AppCompatActivity {
         ,R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
+        //barChart
+        mpBarChart=findViewById(R.id.barChart);
+        BarDataSet barDataSet1=new BarDataSet(barEntries1(),"CO2");
+        barDataSet1.setColors(Color.RED);
+        BarDataSet barDataSet2=new BarDataSet(barEntries2(),"Humidity");
+        barDataSet2.setColors(Color.BLUE);
+        BarDataSet barDataSet3=new BarDataSet(barEntries3(),"Luminosity");
+        barDataSet3.setColors(Color.YELLOW);
+        BarDataSet barDataSet4=new BarDataSet(barEntries4(),"Stupidity");
+        barDataSet4.setColors(Color.GREEN);
+
+        BarData data = new BarData(barDataSet1,barDataSet2,barDataSet3,barDataSet4);
+        mpBarChart.setData(data);
+
+        String[] days=new String[]{"Sunday","Monday","Tuesday","Gayday","Thursday","Friday","Saturday"};
+        XAxis xAxis=mpBarChart.getXAxis();
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(days));
+        xAxis.setCenterAxisLabels(true);
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setGranularity(1);
+        xAxis.setGranularityEnabled(true);
+
+        mpBarChart.setDragEnabled(true);
+        mpBarChart.setVisibleXRangeMaximum(3);
+
+        float barSpace=0.05f;
+        float groupSpace=0.16f;
+        data.setBarWidth(0.16f);
+
+        mpBarChart.getXAxis().setAxisMinimum(0);
+        mpBarChart.getXAxis().setAxisMaximum(0+mpBarChart.getBarData().getGroupWidth(groupSpace,barSpace)*7);
+        mpBarChart.getAxisLeft().setAxisMinimum(0);
+
+        mpBarChart.groupBars(0,groupSpace,barSpace);
+
+        mpBarChart.invalidate();
     }
+
+    private ArrayList<BarEntry> barEntries1()
+    {
+        ArrayList<BarEntry>barEntries=new ArrayList<>();
+        barEntries.add(new BarEntry(1,900));
+        barEntries.add(new BarEntry(2,500));
+        barEntries.add(new BarEntry(3,2000));
+        barEntries.add(new BarEntry(4,1500));
+        barEntries.add(new BarEntry(5,690));
+        barEntries.add(new BarEntry(6,2400));
+        barEntries.add(new BarEntry(7,1700));
+        return barEntries;
+    }
+    private ArrayList<BarEntry>barEntries2()
+    {
+        ArrayList<BarEntry>barEntries=new ArrayList<>();
+        barEntries.add(new BarEntry(1,300));
+        barEntries.add(new BarEntry(2,900));
+        barEntries.add(new BarEntry(3,500));
+        barEntries.add(new BarEntry(4,1000));
+        barEntries.add(new BarEntry(5,2000));
+        barEntries.add(new BarEntry(6,1000));
+        barEntries.add(new BarEntry(7,1200));
+        return barEntries;
+    }
+    private ArrayList<BarEntry>barEntries3()
+    {
+        ArrayList<BarEntry>barEntries=new ArrayList<>();
+        barEntries.add(new BarEntry(1,700));
+        barEntries.add(new BarEntry(2,1000));
+        barEntries.add(new BarEntry(3,300));
+        barEntries.add(new BarEntry(4,1200));
+        barEntries.add(new BarEntry(5,2100));
+        barEntries.add(new BarEntry(6,1300));
+        barEntries.add(new BarEntry(7,1500));
+        return barEntries;
+    }
+    private ArrayList<BarEntry>barEntries4()
+    {
+        ArrayList<BarEntry>barEntries=new ArrayList<>();
+        barEntries.add(new BarEntry(1,900));
+        barEntries.add(new BarEntry(2,1400));
+        barEntries.add(new BarEntry(3,100));
+        barEntries.add(new BarEntry(4,1600));
+        barEntries.add(new BarEntry(5,2500));
+        barEntries.add(new BarEntry(6,1500));
+        barEntries.add(new BarEntry(7,1100));
+        return barEntries;
+    }
+
 
     @Override
     public void onBackPressed() {
